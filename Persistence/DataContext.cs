09f289e5338a0429a4abaacc41ebc9813b81700c
johnpiapian <johnpiapian@gmail.com>
2020-10-11 20:19:48 -0500

    Migrated data

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+
+    public class DataContext: DbContext
+    {
+        public DbSet<Value> Values { get; set;}
+        
+        public DataContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+    }
+}
