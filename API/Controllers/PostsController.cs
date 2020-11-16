d39308a323bdc69fbb007979ec2f9013c0a4a593
Chueseng Vang <chewyvang@Chuesengs-MacBook-Pro.local>
2020-11-16 11:47:36 -0600

    Completed Activty 9

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -101,5 +101,33 @@ namespace API.Controllers
 
             throw new Exception("Error updating post");
         }
+
+        ///<summary>
+        ///DELETE api/posts/[id]
+        ///<sumary/> 
+        ///<param name="id">Post id</Param>
+        ///<returns>True, if successful</returns>
+        [HttpDelete("{id}")]
+
+        public ActionResult<bool> Delete(Guid id)
+        {
+            var post = context.Posts.Find(id);
+
+            if (post == null)
+            {
+                throw new Exception("Could not find post");
+            }
+
+            context.Remove(post);
+
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return true;
+            }
+
+            throw new Exception("Error deleting post");
+        }
     }
 }
