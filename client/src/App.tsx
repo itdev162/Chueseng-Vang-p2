dcc541320be0d4b711a46bbf938118ac45245d1a
Chueseng Vang <chewyvang@Chuesengs-MacBook-Pro.local>
2020-11-29 20:20:32 -0600

    Completed Activity-10

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PostList from './components/PostList/PostList';
 import Post from './components/Post/Post';
+import CreatePost from './components/Post/CreatePost';
+import EditPost from './components/Post/EditPost';
 import './App.css';
 
 class App extends React.Component {
@@ -45,25 +47,60 @@ class App extends React.Component {
       })
   };
 
+  editPost = post => {
+    this.setState({
+      post: post
+    });
+  };
+
+  onPostCreated = post => {
+    const newPosts = [...this.state.posts, post];
+
+    this.setState({
+      posts: newPosts
+    });
+  };
+
+  onPostUpdated = post => {
+    console.log('updated post: ', post);
+    const newPosts = [...this.state.posts];
+    const index = newPosts.findIndex(p => p.id === post.id);
+
+    newPosts[index] = post;
+
+    this.setState({
+      posts: newPosts
+    });
+  };
+
   render() {
     const { posts, post } = this.state;
  
     return (
       <Router>
         <div className="App">
-          <header className="App-Header">
-            BlogBox
-          </header>
+          <header className="App-Header">BlogBox</header>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/new-post">New Post</Link>
+          </nav>
           <main className="App-Content">
             <Switch>
               <Route exact path="/">
                 <PostList posts={posts} 
                           clickPost={this.viewPost}
                           deletePost={this.deletePost}
+                          editPost={this.editPost}
                           />
               </Route>
               <Route path="/posts/:postId">
                 <Post post={post}/>
+              </Route>
+              <Route path="/new-post">
+                <CreatePost onPostCreated={this.onPostCreated} />
+              </Route>
+              <Route path="/edit-post/:postId">
+                <EditPost post={post} onPostUpdated/>
               </Route>
             </Switch>
           </main>
@@ -71,7 +108,7 @@ class App extends React.Component {
       </Router>     
     );
   }
-  
 }
 
 export default App;   
+
--- client/src/components/PostList/PostListItem.js
+++ client/src/components/PostList/PostListItem.js
@@ -4,7 +4,7 @@ import slugify from 'slugify';
 import './styles.css';
 
 const PostListItem = props => {
-    const {post, clickPost, deletePost} = props;
+    const {post, clickPost, deletePost, editPost} = props;
     const history = useHistory();
 
     const handleClickPost = post => {
@@ -14,6 +14,11 @@ const PostListItem = props => {
         history.push(`/posts/${slug}`);
     }
 
+    const handleEditPost = post => {
+        editPost(post);
+        history.push(`/edit-post/${post.id}`);
+    };
+
     return (
         <div>
             <div className="postListItem" onClick={() => handleClickPost(post)}>
@@ -22,6 +27,7 @@ const PostListItem = props => {
             </div>
             <div className="postControls">
                 <button onClick={() => deletePost(post)}>Delete</button>
+                <button onClick={() => handleEditPost(post)}>Edit</button>
             </div>
         </div>
     );
